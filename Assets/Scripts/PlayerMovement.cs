4ed66915514d7e0719394ef958a4393ba30e3de0
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2025-06-09 12:00:50 +0100

    Updated player management

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,22 @@ public class PlayerMovement : MonoBehaviour
     [Header("References")]
     [SerializeField] Rigidbody m_Body;
 
-    [Header("Floor sections")]
-    [SerializeField] GameObject m_StartFloorObject;
-    [SerializeField] GameObject[] m_FloorObjects;
+    [Header("Settings")]
+    [SerializeField] float m_FowardsForce;
+    [SerializeField] float m_SideForce;
 
     // Member variables //
     float m_Force = 0;
+
+    // Only instance of player with a getter //
+    static PlayerMovement s_Instance = null;
+    public PlayerMovement Instance() => s_Instance;
+
+    private void Start()
+    {
+        Debug.Assert(s_Instance == null, "Multiple player instances", this);
+        s_Instance = this;
+    }
 
     private void Update()
     {
