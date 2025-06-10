89ea8c9b983000ed1a3fed886b2b5a68828d77a6
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2025-06-10 11:46:15 +0100

    Updated collision

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -5,12 +5,6 @@ public class Interactable : MonoBehaviour
 {
     [SerializeField] UnityEvent m_Event;
 
-    public void OnInteract()
-    {
-        // Invokes the event if it is valid //
-        if (m_Event != null)
-        {
-            m_Event.Invoke();
-        }
-    }
+    // Invokes the event if it is valid //
+    public void OnInteract() => m_Event?.Invoke();
 }
