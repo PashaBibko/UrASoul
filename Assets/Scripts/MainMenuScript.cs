7e6edfd34cc0250a76bacae518d55f98ee386f02
MrMaker875 <ashtonrobinsonclarke@gmail.com>
2025-06-10 10:13:42 +0100

    Updated menu script

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuScript : MonoBehaviour
@@ -7,12 +5,23 @@ public class MainMenuScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log("PlayGame");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("ExitGame");
     }
 }
